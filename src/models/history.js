676696b0d343ee4493583f4ea00f1d1ae3d17f16
Huy Tran <tranhoanghuy489@gmail.com>
2024-09-13 01:04:09 +0700

    update 13-9

--- src/models/history.js
+++ src/models/history.js
@@ -1,7 +1,7 @@
 "use strict";
 const { Model, BOOLEAN } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class History extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,23 +11,17 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init(
+  History.init(
     {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      address: DataTypes.STRING,
-      phonenumber: DataTypes.STRING,
-      gender: DataTypes.BOOLEAN,
-      image: DataTypes.STRING,
-      roleId: DataTypes.STRING,
-      positionId: DataTypes.STRING,
+      patienId: DataTypes.INTEGER,
+      doctorId: DataTypes.INTEGER,
+      description: DataTypes.TEXT,
+      files: DataTypes.TEXT,
     },
     {
       sequelize,
-      modelName: "User",
+      modelName: "History",
     }
   );
-  return User;
+  return History;
 };
--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -5,9 +5,14 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Users", [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "example@example.com",
+        email: "admin@gmail.com",
+        password: "123456", //plain text   sksjljlkhl -> hash password
+        firstName: "Tran",
+        lastName: "Hoang Huy",
+        address: "BacLieu",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
