e87986574f29c04963204bb1235034bcc802b76e
Huy Tran <tranhoanghuy489@gmail.com>
2024-09-27 07:59:43 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -27,9 +27,7 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
   let data = await CRUDService.getAllUser();
-  console.log("------------------");
-  console.log(data);
-  console.log("------------------");
+
   return res.render("displayCRUD.ejs", {
     dataTable: data,
   });
@@ -60,6 +58,16 @@ let putCRUD = async (req, res) => {
   });
 };
 
+let deleteCRUD = async (req, res) => {
+  let id = req.query.id;
+  if (id) {
+    await CRUDService.deleteUserById(id);
+    return res.send("Delete the user succeed!");
+  } else {
+    return res.send("User not found!");
+  }
+};
+
 // object:{
 // key:"",
 // value:"""
@@ -72,4 +80,5 @@ module.exports = {
   displayGetCRUD: displayGetCRUD,
   getEditCRUD: getEditCRUD,
   putCRUD: putCRUD,
+  deleteCRUD: deleteCRUD,
 };
